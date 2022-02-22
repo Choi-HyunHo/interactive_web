80de8c98e0aa7e24f655f3bb41d136640efdb39e
com <x7788@naver.com>
2022-02-22 23:44:15 +0900

    mouse_move 부드럽게 하기

--- mouse_move.js
+++ mouse_move.js
@@ -1,11 +1,26 @@
 const title = document.querySelector('h1')
-const box = document.querySelector('.box')
-const boxRect = box.getBoundingClientRect()
+let box
+let x = 0 
+let y = 0 
+let mouseX = 0
+let mouseY = 0
+let speed = 0.03
 
-const boxWidth = boxRect.width / 2
-const boxHeight = boxRect.height 
 
-window.addEventListener('mousemove',(e)=>{
-    title.innerHTML = `X : ${e.clientX}, Y : ${e.clientY}`
-    box.style.transform = `translate(${e.clientX - boxWidth}px, ${e.clientY- boxHeight}px)`
-})
+window.onload = function(){
+    box = document.querySelector('.box')
+    window.addEventListener('mousemove', mouseMove)
+    function mouseMove(e){
+         x = e.clientX
+         y = e.clientY
+        title.innerHTML = `X : ${x}, Y : ${y}`
+    }
+    loop()
+}
+
+function loop() {
+    mouseX += (x-mouseX) * speed
+    mouseY += (y-mouseY) * speed
+    box.style.transform = `translate(${mouseX}px, ${mouseY}px)`
+    window.requestAnimationFrame(loop)
+}
